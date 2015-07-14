3d65ea1ddcb31d12acc5f9cc9419ed77ada4cea4
airtoxin <airtoxin@icloud.com>
2015-07-14 18:03:47 +0900

    Add current_track test

--- libs/current_track.js
+++ libs/current_track.js
@@ -66,28 +66,29 @@ var properties = [
 ];
 var OSA_BASE = 'Application("iTunes").currentTrack';
 
-var cast = function (data) {
-	if (data.match(/^\d+(\.\d+)?$/)) return Number(data);
-	if (data.match(/^null$/)) return null;
-	if (data.match(/^(true|false)$/)) return data === 'true';
-	if (data.match(/^\w{3}\s\w{3}\s\d{2}\s\d{4}\s\d{2}:\d{2}:\d{2}\s.*$/)) return new Date(data);
-	return data;
-};
-
 module.exports = {
+	osascript: require('osascript').eval,
 	main: function (callback) {
+		var self = this;
 		if (typeof callback !== 'function') callback = function(){};
 
 		var result = {};
 		async.each(properties, function (property, next) {
 			var script = OSA_BASE + '.' + property + '();';
-			osascript(script, function (error, data) {
+			self.osascript(script, function (error, data) {
 				if (error) return next(error);
-				result[property] = cast(data.trim());
+				result[property] = self.cast(data.trim());
 				next();
 			});
 		}, function (error) {
 			callback(error, result);
 		});
+	},
+	cast: function (data) {
+		if (data.match(/^-?\d+(\.\d+)?$/)) return Number(data);
+		if (data.match(/^null$/)) return null;
+		if (data.match(/^(true|false)$/)) return data === 'true';
+		if (data.match(/^\w{3}\s\w{3}\s\d{2}\s\d{4}\s\d{2}:\d{2}:\d{2}\s.*$/)) return new Date(data);
+		return data;
 	}
 };
