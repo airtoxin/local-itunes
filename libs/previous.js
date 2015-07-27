94742be4ba561a12995d1bd49e65335b91c21ffa
Mario Cecchi <macecchi@gmail.com>
2015-07-26 22:18:27 -0300

    Add previous and next calls

---
+++ libs/previous.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var async = require('neo-async');
+var agent = require(path.join(__dirname, 'utils', 'agent'));
+
+module.exports = {
+	agent: agent,
+	methodPath: 'backTrack',
+	main: function (callback) {
+		this.agent.execute(this.methodPath, callback);
+	}
+};
